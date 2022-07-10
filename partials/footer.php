d03e02d9cb0167941c4c0d33144c810919483236
JEDAGUEMU0331 <jedaguemu0331@gmail.com>
2022-07-10 15:20:02 -0500

    Creating of components and home page.

---
+++ partials/footer.php
@@ -0,0 +1,20 @@
+        <footer class="bg-dark text-center mt-3" style="border-radius: 0px 0px 25px 25px; color: silver;">
+
+            <!-- Copyright -->
+            <div class="text-center p-3 lh-1" style="border-radius: 0px 0px 25px 25px;">
+                <strong>
+                    Contáctenos <br>
+                    Línea gratuita 018000-00001 <br>
+                    Banco entidad financiera - Todos lo derechos reservados.
+                </strong>
+            </div>
+            <!-- Copyright -->
+        </footer>
+    
+    </div> <!-- Div.container initialized in header.php -->
+   
+   <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    </body>
+</html>
---
+++ partials/nabvar.php
@@ -0,0 +1,30 @@
+    <div class="card text-center" style="border-radius: 25px; border: none;">
+        <div class="card-header p-3" style="border-radius: 25px 25px 0px 0px; background: silver;">
+            <div class="row">
+                <div class="col-8 lh-1">
+                    <div class="card-text" style="font-family: Arial Black; font-size: 60px; -webkit-text-fill-color: black; -webkit-text-stroke: 3px; -webkit-text-stroke-color: #EAEDED;">
+                        BANCOOP
+                    </div>
+                    <strong class="card-text fs-3">Te ayuda a alcanzar tus metas</strong>
+                </div>
+                <div class="col-4 justify-content-center  align-items-center">
+                    <a href="/Bancoop/index.php">
+                        <img class="img-fluid" src="/Bancoop/img/Bancoop.png" alt="" style="max-height: 90px;">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="bg-dark" style="height: 10px;">
+        </div>
+        <div class="card-footer p-3" style="border-radius: 0px 0px 25px 25px; background: #FFC300;">
+            <nav class="nav nav-pills flex-column flex-sm-row">
+                <a class="flex-sm-fill text-sm-center nav-link" href="/Bancoop/views/credits.php"><strong class="fs-5 text-dark">.Créditos.</strong></a>
+                <div style="background-color: white; width: 3px;"></div>
+                <a class="flex-sm-fill text-sm-center nav-link" href="/Bancoop/views/leasing.php"><strong class="fs-5 text-dark">.Leasing.</strong></a>
+                <div style="background-color: white; width: 3px;"></div>
+                <a class="flex-sm-fill text-sm-center nav-link" href="/Bancoop/views/savings.php"><strong class="fs-5 text-dark">.Ahorros.</strong></a>
+                <div style="background-color: white; width: 3px;"></div>
+                <a class="flex-sm-fill text-sm-center nav-link" href="/Bancoop/views/customerService.php"><strong class="fs-5 text-dark">.Servicio al cliente.</strong></a>
+            </nav>            
+        </div>
+    </div>
